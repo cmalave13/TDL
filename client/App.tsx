4efdc1c2a1ec9f5b62bc02ac2401c7d2cad6ef12
Charlie Malave <malavecharles@gmail.com>
2021-09-08 13:53:24 -0400

    setup tdl boiler plate

--- client/App.tsx
+++ client/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import axios from 'axios';
 import './styles.css';
-import IMAGE from './assets/Pierres_shop.png';
+import IMAGE from './assets/tdl.png';
 
 // export interface user {
 //   username: string
@@ -9,69 +9,14 @@ import IMAGE from './assets/Pierres_shop.png';
 
 export const App:React.FC = () => {
   
-   const [username, setUsername] = useState('');
-   const [password, setPassword] = useState('');
-   const [loggedIn, setLoggedIn] = useState(false);
 
-   const login = (e:any) => {
-     e.preventDefault();
-     if(loggedIn){
-       console.log('You are already logged in!');
-       return;
-     }
-     axios.post("/api/login", {
-      data: {
-        username: username,
-        password: password,
-      },
-    })
-    .then((res) => {
-      console.log(res.data);
-      if(res.data.message === 'You successfully logged in!'){
-        setLoggedIn(true);
-      }
-    })
-    .catch((err) => console.log(err));
-   }
-
-   const register = (e:any) => {
-    e.preventDefault();
-    axios.post("/api/register", {
-     data: {
-       username: username,
-       password: password,
-     },
-   })
-   .then((res) => {
-     console.log(res.data);
-     
-   })
-   .catch((err) => console.log(err));
-  }
 
 
     return (
     <>
-      <h1>Pierre's Store</h1>
-      <img src={IMAGE} alt="Pierre's Store" width="300" height="200" />
-        <form>
-        <input
-          id="taskForm"
-          type="form"
-          placeholder="Username"
-          onChange={(e) => setUsername(e.target.value)}
-        />
-        <input
-          id="taskForm"
-          type="form"
-          placeholder="Password"
-          onChange={(e) => setPassword(e.target.value)}
-        /> 
-        <button id="loginBtn" className="submitTask" onClick={login}>Login</button>
-        <button id="registerBtn" className="submitTask" onClick={register}>Register</button>
-      </form>
-      <img src="https://pierresstore.s3.us-east-2.amazonaws.com/Grass_Starter.png"/>
-       
+    <img src={IMAGE} alt="Pierre's Store" width="300"  />
+      <h1>Add Tasks Below</h1>
+      
     </>
   )
 }
--- server/server.js
+++ server/server.js
@@ -1,51 +1,17 @@
-const path = require('path');
-const express = require('express');
-const app = express();
-const apiRouter = require('./routes/api');
-const mongoose = require("mongoose");
-const env = require('dotenv').config();
-const session = require('express-session');
-const mongoStore = require('connect-mongodb-session')(session);
-const PORT = process.env.PORT;
-// const cors = require('cors')
+const path = require('path')
+const express = require('express')
+const app = express()
+const env = require('dotenv').config()
+const session = require('express-session')
+const PORT = process.env.PORT || 3000
 
-
-
-app.use(express.json());
-
+//call API Router when needed (for middleware) --> // const apiRouter = require('./routes/api')
+// Route Handlers
+//app.use('/api', apiRouter)
+app.use(express.json())
 
 // Parses incoming requests with urlencoded payloads
-app.use(express.urlencoded({ extended: true }));
-
-app.use(session({
-  key: process.env.COOKIE_KEY,
-  secret: process.env.SESSION_SECRET,
-  resave: true,
-  saveUninitialized: false,
-  store: new mongoStore({
-    uri: `${process.env.URI}`,
-    databaseName: 'user',
-    collection: 'sessions',
-    expires: 60 * 60
-  }),
-  cookie: {
-    maxAge: 900000
-  }
-}))
-
-//mongoDB
-mongoose.connect(`${process.env.URI}`, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  dbName: 'user'
-});
-
-mongoose.connection.once("open", () => {
-  console.log("Connected to MongoDB");
-});
-
-// Route Handlers
-app.use('/api', apiRouter);
+app.use(express.urlencoded({ extended: true }))
 
 //Default Error Handler
 
@@ -54,24 +20,24 @@ app.use((err, req, res, next) => {
     log: 'Express error handler caught unknown middleware error',
     status: 400,
     message: { err: 'An error occurred' },
-  };
-  const errorObj = Object.assign({}, defaultErr, err);
-  console.log(errorObj.log);
-  return res.status(errorObj.status).json(errorObj.message);
-});
+  }
+  const errorObj = Object.assign({}, defaultErr, err)
+  console.log(errorObj.log)
+  return res.status(errorObj.status).json(errorObj.message)
+})
 
 // statically serve everything in the build folder on the route '/build'
-app.use('/build', express.static(path.join(__dirname, '../build')));
+app.use('/build', express.static(path.join(__dirname, '../build')))
 // serve index.html on the route '/'
 app.get('/', (req, res) => {
-  return res.sendFile(path.join(__dirname, '../index.html'));
-});
+  return res.sendFile(path.join(__dirname, '../index.html'))
+})
 
 // Catch-all to unknown routes (404)
-app.use((req,res) => res.status(404).send('not found'))
+app.use((req, res) => res.status(404).send('not found'))
 //Start Server
 app.listen(PORT, () => {
-  console.log(`Server listening on port: ${PORT}`);
-});
+  console.log(`Server listening on port: ${PORT}`)
+})
 
-module.exports = app;
+module.exports = app
